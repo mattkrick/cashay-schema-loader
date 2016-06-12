89fb1d58838961adcf7bc9f364c99dfd1f55439e
Simen Brekken <simen@unfold.no>
2016-06-12 08:25:46 +0200

    Work around multiple GraphQL version problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import 'babel-polyfill';
 import path from 'path';
-import { graphql } from 'graphql';
 import introspectionQuery from './introspectionQuery';
 import makeMinimalSchema from './makeMinimalSchema';
 
+const graphql = require(path.join(process.cwd(), 'node_modules', 'graphql')).graphql;
+
 export default async function(source) {
   this.cacheable();
-  
+
   const callback = this.async();
   const schema = this.exec(source, this.resourcePath);
   const initialResult = await graphql(schema, introspectionQuery);
