6d07b3b6b356f95f7746a922708a4b809c90e9d8
Simen Brekken <simen@unfold.no>
2016-06-12 08:23:45 +0200

    Make it build

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import 'babel-polyfill';
 import path from 'path';
+import { graphql } from 'graphql';
 import introspectionQuery from './introspectionQuery';
 import makeMinimalSchema from './makeMinimalSchema';
 
-const graphql = require(path.join(process.cwd(), 'node_modules', 'graphql')).graphql;
-
-export default async function() {
+export default async function(source) {
+  this.cacheable();
+  
   const callback = this.async();
   const schema = this.exec(source, this.resourcePath);
   const initialResult = await graphql(schema, introspectionQuery);
   const minimalSchema = makeMinimalSchema(initialResult.data.__schema);
   const result = `module.exports = ${JSON.stringify(minimalSchema)};`;
-  //
+
   callback(null, result)
 };
